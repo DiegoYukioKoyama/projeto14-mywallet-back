33ae3159bc89a12c2f3b4693e8945af839638f17
DiegoKoyama <yukioko.jp@gmail.com>
2023-01-22 00:35:51 -0300

    servior implementado

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+
+server.use(express.json());
+
+server.use(cors());
+
+const PORT = 5000;
+
+server.listen(PORT, () => {console.log("funfou")})
